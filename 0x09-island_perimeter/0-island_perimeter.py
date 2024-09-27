634d56d343e70ee2960a545f1dfb8f77f7ff271b
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-09-27 10:28:33 +0100

    fixing bug

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -18,11 +18,11 @@ def island_perimeter(grid):
 
                 if q > 0 and grid[q - 1][w] == 1:  # check above
                     perimeter -= 1  # remove the corner that is there
-                if q < row and grid[q + 1][w] == 1:  # check below and remove
+                if q < row - 1 and grid[q + 1][w] == 1:  # check below and remove
                     perimeter -= 1
                 if w > 0 and grid[q][w - 1] == 1:  # check left
                     perimeter -= 1
-                if w < col and grid[q][w + 1] == 1:
+                if w < col - 1 and grid[q][w + 1] == 1:
                     perimeter -= 1
 
     return perimeter
